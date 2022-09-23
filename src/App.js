ea51b247ceabaa71a4f014b7ac46154abe686ca2
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-23 17:42:57 -0400

    Build out and style header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css"
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey Ethan</h1>
+      <Router>
+        <Routes>
+          <Route path="/search" element={<h1>Search page</h1>} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
