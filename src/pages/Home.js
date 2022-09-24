ce70d16730d470cc5cea70ba2d0454800241e498
ethansalonga <ethan@ethansalonga.dev>
2022-09-24 19:26:45 -0400

    Add logo, search bar, and buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import "./Home.css"
 import { Link } from "react-router-dom"
 import AppsIcon from '@mui/icons-material/Apps'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Search from "../Search";
 
 function Home() {
   return (
@@ -19,7 +20,10 @@ function Home() {
         </div>
       </div>
       <div className="home__body">
-
+        <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
       </div>
     </div>
   )
