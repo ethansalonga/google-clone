f00ad0f1690c0b7ca83a2a228c2bde5437cb0e1d
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-30 19:53:42 -0400

    Create sample response

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,17 @@
 import "./SearchPage.css"
 import { useStateValue } from "../StateProvider"
+import useGoogleSearch from "../useGoogleSearch"
+import Response from "../response"
 
 const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue()
 
+  // LIVE API CALL
+  // const { data } = useGoogleSearch(term)
+
+  const data = Response
+
+  console.log(data)
   return (
     <div className="searchPage">
       <div className="searchPage__header">
