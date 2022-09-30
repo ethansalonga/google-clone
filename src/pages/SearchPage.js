ddc40b07900e8f506850e3b6850b783926aade70
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-30 19:39:45 -0400

    Create useGoogleSearch custom hook

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,9 @@
-import React from 'react'
 import "./SearchPage.css"
 import { useStateValue } from "../StateProvider"
 
 const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue()
 
-  // 027477d4205b64326
   return (
     <div className="searchPage">
       <div className="searchPage__header">
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import API_KEY from "./keys"
 
-const useGoogleSearch = () => {
-  return (
-    <div>useGoogleSearch</div>
-  )
+const CONTEXT_KEY = "027477d4205b64326"
+
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then(response => response.json())
+        .then(result => {
+          setData(result)
+        })
+    }
+
+    fetchData()
+  }, [term])
+
+  return { data }
 }
 
 export default useGoogleSearch
