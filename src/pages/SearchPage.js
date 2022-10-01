5a6f5964c2f3d4bd218a63444c0a0ce85379016e
Ethan Salonga <ethansalonga28@gmail.com>
2022-10-01 16:19:41 -0400

    Build search page results

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -15,11 +15,11 @@ const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue()
 
   // LIVE API CALL
-  // const { data } = useGoogleSearch(term)
+  const { data } = useGoogleSearch(term)
 
-  const data = Response
+  // MOCK API CALL
+  // const data = Response
 
-  console.log(data)
   return (
     <div className="searchPage">
       <div className="searchPage__header">
@@ -67,6 +67,32 @@ const SearchPage = () => {
           </div>
         </div>
       </div>
+
+      {term && (
+        <div className="searchPage__results">
+          <p className="searchPage__resultCount">
+            About {data?.searchInformation.formattedTotalResults} results in ({data?.searchInformation.formattedSearchTime} seconds) for {term}
+          </p>
+
+          {
+            data?.items.map(item => (
+              <div className="searchPage__result">
+                <a href={item.link}>
+                  {item.pagemap?.cse_image?.length > 0 &&
+                    item.pagemap?.cse_image[0]?.src && (
+                      <img className="searchPage__resultImage" src={item.pagemap?.cse_image[0]?.src} />
+                    )}
+                  {item.displayLink} ▽
+                </a>
+                <a href={item.link} className="searchPage__resultTitle">
+                  <h2>{item.title}</h2>
+                </a>
+                <p className="searchPage__resultSnippet">{item.snippet}</p>
+              </div>
+            ))
+          }
+        </div>
+      )}
 
       <div className="searchPage__results">
 
